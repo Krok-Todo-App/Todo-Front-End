ca702d0ff71b13de0ac562d3eae8afd5e10bd4e1
eminzzz <eminroth@gmail.com>
2021-04-25 18:35:10 +0300

    add layouts

---
+++ src/layouts/AuthLayout.vue
@@ -0,0 +1,13 @@
+<template>
+  <slot/>
+</template>
+
+<script>
+export default {
+  name: "AuthLayout"
+}
+</script>
+
+<style scoped>
+
+</style>
---
+++ src/layouts/MainLayout.vue
@@ -0,0 +1,13 @@
+<template>
+  <slot/>
+</template>
+
+<script>
+export default {
+  name: "MainLayout"
+}
+</script>
+
+<style scoped>
+
+</style>
